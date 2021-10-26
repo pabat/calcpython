72b49d3eab741601268f5dc7a4dfb4e7860dc983
SergeyYn <31241884+SergeyYn@users.noreply.github.com>
2021-10-27 00:17:38 +0300

    Update python_ml_calculator.py

--- python_ml_calculator.py
+++ python_ml_calculator.py
@@ -14,6 +14,10 @@ Please type in the math operation you would like to complete:
     if operation == '+':
         print('{} + {} = '.format(number_1, number_2))
         print(number_1 + number_2)
+        
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
 
     else:
         print('You have not typed a valid operator, please run the program again.')
